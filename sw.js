99321015941aac50ef07f573a5fcda48ee6ce74e
jenifersosa123334 <232411088@cecytejalisco.edu.mx>
2026-04-23 20:19:37 -0600

    susurro
    susurros anonimo

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'susurros-v2';
+const ASSETS = ['/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
+});
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
